7b67769b89d6cbae2ddcbe4dee56f6d842abfb96
Josue Perez <joshua72.perez@gmail.com>
2020-02-24 21:22:52 -0600

    Lista simple
    metodos:
    insertar
    buscar
    eliminar

--- Tareas/T3/lista_simple.cpp
+++ Tareas/T3/lista_simple.cpp
@@ -3,9 +3,9 @@
 //------- NODO----------
 nodo::nodo(string name , int ident)
 {
-    nombre = name;
-    id_estudiante = ident;
-    siguiente = NULL;
+    this->nombre = name;
+    this->id_estudiante = ident;
+    this->siguiente = NULL;
 }
 
 //---------------------------
@@ -13,5 +13,50 @@ nodo::nodo(string name , int ident)
 //-------LISTA---------------
  
  lista_simple:: lista_simple(){
-     primero = NULL;
+     this->primero = NULL;
+ }
+
+
+ void lista_simple::insertar(int id , string nombre){
+     nodo * nuevo = new nodo(nombre, id);
+
+    if(this->primero==NULL){
+        this->primero = nuevo;
+    }else{
+        nuevo->siguiente = this->primero;
+        this->primero = nuevo;
+    }
+ }
+ 
+ void lista_simple::eliminar(nodo * nodo_eliminar){
+     nodo * aux = this->primero;
+
+    if(nodo_eliminar == this->primero){
+        this->primero = this->primero->siguiente;
+        aux->siguiente = NULL;
+    }else{
+        nodo * aux2 = this->primero;
+
+        while(aux != NULL){
+            if(aux == nodo_eliminar){
+                aux2->siguiente = aux->siguiente;
+                aux->siguiente = NULL;
+            }else{
+                aux2 = aux;
+                aux= aux->siguiente;
+            }
+        }
+    }
+ }
+ 
+ nodo * lista_simple::buscar(int carnet_buscar){
+     nodo * aux = this->primero;
+     while(aux != NULL){
+         if(aux->id_estudiante == carnet_buscar){
+             return aux;
+         }else{
+             aux= aux->siguiente;
+         }
+     }
+     return NULL; // SI NO LO ENCUENTRA RETORNARA NULL
  }
